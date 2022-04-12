b7fddeed2f6fb10e848e817da99afa56dab9cfdc
izzybrandagility <97474221+izzybrandagility@users.noreply.github.com>
2022-04-12 13:10:33 -0700

    skip pwd command

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@ from subprocess import check_call
 
 class PostDevelopCommand(develop):
     def run(self):
-        check_call("pwd | say".split())
         check_call("./downward/build.py")
         develop.run(self)
 
 class PostInstallCommand(install):
     def run(self):
-        check_call("pwd | say".split())
         check_call("./downward/build.py")
         install.run(self)
 
