1ba50d36c699ad9dc48b1f5b5a2b06c810d5320f
izzybrandagility <97474221+izzybrandagility@users.noreply.github.com>
2022-04-12 12:55:19 -0700

    trying to call a post-setup command

--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+class PreDevelopCommand(develop):
+    """Pre-installation for development mode."""
+    def run(self):
+#         check_call("apt-get install this-package".split())
+        print("HELLO")
+        develop.run(self)
+
+class PreInstallCommand(install):
+    """Pre-installation for installation mode."""
+    def run(self):
+#         check_call("apt-get install this-package".split())
+        print("HELLO")
+        install.run(self)
+
+
 setup(name='pddlstream',
       version='1.0',
       py_modules=['pddlstream'],
-      )
+      cmdclass={
+          'develop': PostDevelopCommand,
+          'install': PostInstallCommand,
+      }
+)
