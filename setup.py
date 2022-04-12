64ca8f72dc77dfe8329e6cadba0ddc48d1afb161
izzybrandagility <97474221+izzybrandagility@users.noreply.github.com>
2022-04-12 13:08:42 -0700

    trying to call a subprocess

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from subprocess import check_call
 
 class PostDevelopCommand(develop):
     def run(self):
-#         check_call("apt-get install this-package".split())
-        print("HELLO")
+        check_call("pwd | say".split())
+        check_call("./downward/build.py"))
         develop.run(self)
 
 class PostInstallCommand(install):
     def run(self):
-#         check_call("apt-get install this-package".split())
-        print("HELLO")
+        check_call("pwd | say".split())
+        check_call("./downward/build.py"))
         install.run(self)
 
 
