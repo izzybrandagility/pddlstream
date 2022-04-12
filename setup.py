3d9dfe3ce360666715a96e39b3af5ff3f301a905
izzybrandagility <97474221+izzybrandagility@users.noreply.github.com>
2022-04-12 13:53:59 -0700

    change to package

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
 from setuptools import setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-from subprocess import check_call
-
-
-class PostDevelopCommand(develop):
-    def run(self):
-        check_call("./downward/build.py")
-        develop.run(self)
-
-class PostInstallCommand(install):
-    def run(self):
-        check_call("./downward/build.py")
-        install.run(self)
-
 
 setup(name='pddlstream',
       version='1.0',
-      py_modules=['pddlstream'],
-      cmdclass={
-          'develop': PostDevelopCommand,
-          'install': PostInstallCommand,
-      }
+      packages=['pddlstream']
 )
